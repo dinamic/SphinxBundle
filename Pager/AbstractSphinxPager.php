a3d7fb4efd08e15d5aa00cf5c48cf5ccb948afb4
Nikola Petkanski <dinamic@gmail.com>
2012-04-24 13:32:53 +0300

    ~ updated method for extracting primary keys from sphinx search results

--- Pager/AbstractSphinxPager.php
+++ Pager/AbstractSphinxPager.php
@@ -8,8 +8,9 @@ use Symfony\Component\DependencyInjection\ContainerAware;
  * AbstractSphinxPager
  *
  * @package HighcoSphinBundle
- * @version 0.1
+ * @version 0.2
  * @author Stephane PY <py.stephane1@gmail.com>
+ * @author Nikola Petkanski <nikola@petkanski.com>
  */
 abstract class AbstractSphinxPager extends ContainerAware
 {
@@ -22,9 +23,10 @@ abstract class AbstractSphinxPager extends ContainerAware
     {
         $matches = isset($this->results['matches']) ? $this->results['matches'] : array();
 
-        $pks     = array();
-        foreach ($matches as $id => $match) {
-            $pks[$id] = $id;
+        $pks = array();
+        
+        foreach ($matches as $match) {
+            $pks[] = $match['id'];
         }
 
         return $pks;
