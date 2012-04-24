78eb95c214d66ddef89945d50945431ba3861edc
Nikola Petkanski <dinamic@gmail.com>
2012-04-24 13:30:59 +0300

    ~ set sphinx api to return plain array instead of indexed one

--- Client/DefaultClient.php
+++ Client/DefaultClient.php
@@ -8,9 +8,16 @@ require __DIR__."/../../../../../vendor/sphinx/sphinxapi.php";
  * DefaultClient
  *
  * @package HighcoSphinBundle
- * @version 0.1
+ * @version 0.2
  * @author Stephane PY <py.stephane1(at)gmail.com>
+ * @author Nikola Petkanski <nikola@petkanski.com>
  */
 class DefaultClient extends \SphinxClient
 {
+    public function __construct()
+    {
+        parent::__construct();
+        
+        $this->SetArrayResult(true);
+    }
 }
